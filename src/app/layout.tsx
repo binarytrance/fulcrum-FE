6c7da9889b9562b73c8c4985a5e84f057b22a2ee
sambit24795 <50554112+sambit24795@users.noreply.github.com>
2026-04-14 18:13:06 +0530

    Remove global body padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased p-10`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
